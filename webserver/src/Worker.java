bc444415d181719540e9d6550c792be70e1c2e17
quentinlg <quentin.leguennec@epfl.ch>
2013-03-15 14:57:27 +0100

    J'ai juste mis le StaticSite de déclaré. J'ai un peu compris la forme de la réponse et j'essaierai de faire ça en début de semaine prochaine. En fait faut regarder dans les explications du dessus à quoi ça ressemble. Voila Voila. Bon ski!

--- webserver/src/Worker.java
+++ webserver/src/Worker.java
@@ -3,18 +3,24 @@ import java.net.Socket;
 
 import lsr.concurrence.http.HttpRequest;
 import lsr.concurrence.http.HttpRequestStream;
+import lsr.concurrence.http.HttpResponse;
+import lsr.concurrence.webserver.StaticSite;
 
 
 public class Worker extends Thread{
 	HttpRequest req;
 	HttpRequestStream rStream;
+	HttpResponse rep;
 	Socket socket;
+	StaticSite staticS;
 	public Worker(Socket sock) {
 		socket = sock;
 		// TODO Auto-generated constructor stub 
 		try {
 			rStream = new HttpRequestStream(socket.getInputStream());
 			req = rStream.readRequest();
+			staticS = req.;
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

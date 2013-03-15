e2ce13d2cf420d22481e0004ad2d5702498e85b6
quentinlg <quentin.leguennec@epfl.ch>
2013-03-15 13:30:17 +0100

    merdeier

--- webserver/src/Webserver.java
+++ webserver/src/Webserver.java
@@ -6,7 +6,7 @@ public class Webserver {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLOO");
+		System.out.println("HELLO");
 	}
 
 }

ad5d3b3c82dc26320426a03da04dc1d9d09196a9
kserrano <kevin.serrano@epfl.ch>
2013-03-21 15:45:21 +0100

    comments

--- webserver/src/TCPAcceptor.java
+++ webserver/src/TCPAcceptor.java
@@ -5,6 +5,8 @@ import lsr.concurrence.http.HttpRequestStream;
 
 public class TCPAcceptor extends Thread {
 	ServerSocket serverSocket = new ServerSocket(8080);
+	// read HTTP request from stream
+	// when a request detected -> creat a worker 
 	while (true) {
 
 		serverSocket.accept();
